1721fc7db9e6aa5d79e1fc3bb716cdba720b3a93
alcorralesh <alcorralesh@gmail.com>
2026-07-26 18:36:14 +0200

    Fix Android portrait folder card containment

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Nunca almacena progreso, respuestas autenticadas ni archivos del expediente. */
 'use strict';
 
-const VERSION='20260726-folder-card01';
+const VERSION='20260726-folder-card02';
 const STATIC_CACHE=`kizuna-static-${VERSION}`;
 const PAGE_CACHE=`kizuna-pages-${VERSION}`;
 const KIZUNA_CACHE_PREFIXES=['kizuna-static-','kizuna-pages-'];
