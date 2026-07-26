2311144ad65009cb987f87af1f58189a31bd5b61
alcorralesh <alcorralesh@gmail.com>
2026-07-26 10:48:55 +0200

    Bump PWA version for AT-03 login flow

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Nunca almacena progreso, respuestas autenticadas ni archivos del expediente. */
 'use strict';
 
-const VERSION='20260725-early-access05';
+const VERSION='20260726-auth-at03-login01';
 const STATIC_CACHE=`kizuna-static-${VERSION}`;
 const PAGE_CACHE=`kizuna-pages-${VERSION}`;
 const KIZUNA_CACHE_PREFIXES=['kizuna-static-','kizuna-pages-'];
