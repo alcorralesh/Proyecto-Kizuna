64ca677039cbaca1d256b19e40aecd08294685c6
alcorralesh <alcorralesh@gmail.com>
2026-08-04 19:51:14 +0200

    style: centrar decision en tablet vertical

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Nunca almacena progreso, respuestas autenticadas ni archivos del expediente. */
 'use strict';
 
-const VERSION='20260804-decision-tablet01';
+const VERSION='20260804-decision-tablet03';
 const STATIC_CACHE=`kizuna-static-${VERSION}`;
 const PAGE_CACHE=`kizuna-pages-${VERSION}`;
 const KIZUNA_CACHE_PREFIXES=['kizuna-static-','kizuna-pages-'];
