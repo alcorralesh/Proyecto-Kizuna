5991d1cf1ab150b2384551880130f3665b784778
alcorralesh <alcorralesh@gmail.com>
2026-08-04 20:27:03 +0200

    fix: restaurar acceso al expediente sin sesion

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Nunca almacena progreso, respuestas autenticadas ni archivos del expediente. */
 'use strict';
 
-const VERSION='20260804-finale-state01';
+const VERSION='20260804-loggedout-expedient01';
 const STATIC_CACHE=`kizuna-static-${VERSION}`;
 const PAGE_CACHE=`kizuna-pages-${VERSION}`;
 const KIZUNA_CACHE_PREFIXES=['kizuna-static-','kizuna-pages-'];
